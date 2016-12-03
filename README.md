4a863a0eb1dcba1a43e3efbe5b59ff7902ca1154
Stephen Terry <stephenrterry@msn.com>
2016-12-03 10:48:36 -0800

    Updated design and charts from user feedback.

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ was created in the data so that airports can be compared by the percentage of th
 flights they have.
 
 Design:
+Original Design
 The selected data all generates from the United States.  Therefore, I chose a map of only
 the United States, and used the AlbersUSA projection so that Hawaii and Alaska would appear
 closer to the continental states.
@@ -23,25 +24,86 @@ and the closer to red the higher the percentage of delays.
 A barchart was added to the visualization so that the categories of delays could be investigated
 by users when they click on any airport (this data is only available for years after 2003).
 
+After Review Design
+The size of each circle has been changed to map to the ratio of total flights, not delayed flights. 
+The coloring is still mapped to the percent of delayed flights.
+
+The layout was changed so that the bar chart appeared next to the map when a circle was selected. 
+This was a common review, in that users did not like scrolling up and down to see the map and the chart.
+
+The bar chart was modified so that it would clear when years with no categorical data were selected.
+
 Feedback:
 Reviewer #1:
+
 The animation occurs too fast.
-RESPONSE: Increased the delay between updates.
+
+Increased the delay between updates.
+
 The link to the delay category definitions is not obvious.
-RESPONSE: Changed the link from the entire chart title to only the "Delays by Category".
-The pop-up dialog indicates no category data prior to 2003, but it is unclear if it was specific to 
-a particular airport or all airports.
-RESPONSE: Changed the dialog to indicate all airports.
+
+Changed the link from the entire chart title to only the "Delays by Category".
+
+The pop-up dialog indicates no category data prior to 2003, but it is unclear if it was specific to a particular airport or all airports.
+
+Changed the dialog to indicate all airports.
+
 
 Reviewer #2:
-Nothing in visualization provides info on units.  Are these the number flights,
-number of delays, or what?
-RESPONSE: Added sub-title to indicate that we are looking at the percentage of flights delayed
+
+Nothing in visualization provides info on units. Are these the number flights, number of delays, or what?
+
+Added sub-title to indicate that we are looking at the percentage of flights delayed
+
+The circle colors (green through red) are kind of faded looking.
+
+Changed the opacity of the circles
+
+When selecting years before 2003, a pop-up warns "No delay type data for years before 2003." The graphic still shows the previous data, so there is an inconsistency.
+
+Now plotting zeros for years with no data to clear the plot>
+
+I don't like having to scroll down every time I want to see the breakdown by Delay Category. I would prefer to see a smaller graphic next to the map>
+
+Changed size of graphs, and made them responsive to the page size.
+
 
 Reviewer #3:
+
 The link to the delay category definitions is not obvious.
-RESPONSE: Changed the link from the entire chart title to only the "Delays by Category".
+
+Changed the link from the entire chart title to only the "Delays by Category".
+
 It would be nice to zoom into the map in areas where airports exist in close proximity to another.
+
+
+Reviewer #4:
+
+It may be useful to know the total number of flights for each airport.
+
+Added the total number of flights to mouse hover dialog.
+
+For data before 2003, the plot should clear since no data is available for delay categories.
+
+Now plotting zeros for years with no data to clear the plot.
+
+
+Reviewer #5:
+
+It would be nice if the page was responsive so that it could be viewed on multiple devices.
+
+Changed the SVG's to be responsive to page size.
+
+
+Reviewer #6:
+
+The animation displays data over time, and the static chart displays the delay data for airports for one year. It would be nice to show the data for the selected airport over all years.
+
+Added a line chart to include total flights and delayed flights for all years of the selected airport.
+
+It would be nice if the circle size was related to the total flights and not the delayed flights>
+
+Changed the radius of circles to look at total flights instead of delayed flights
 
 
 Resources:
@@ -49,6 +111,8 @@ References Used:
 Bar Chart:"http://alignedleft.com/tutorials/d3/making-a-bar-chart"
 
           "http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922"
+
+Responsive SVG's: "http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3"
 
 Data Sources:
           Data for years 1987 - 2008: "http://stat-computing.org/dataexpo/2009/the-data.html"
@@ -60,3 +124,6 @@ Code Sources:
 
           GeoJSON: "https://bitbucket.org/john2x/d3test/raw/2ce4dd5112448088fe357b8179d1088ef19524b8/d3/examples/data"
           Note that this file was downloaded and is available in the data directory of the repo.</p>
+
+	  Legend: "http://bl.ocks.org/ZJONSSON/3918369"
+          Note that this file was downloaded and is available in the js directory for this project.</p>
--- js/airports.js
+++ js/airports.js
@@ -1,8 +1,13 @@
     function draw(geo_data) {
       "use strict";
-      var margin = 100,
-          width = 950 - margin,
-          height = 600 - margin;
+      // var margin = 100,
+      //     width = 950 - margin,
+      //     height = 600 - margin;
+      var margin = {top: 10, left: 10, bottom: 10, right: 10}
+        , width = parseInt(d3.select('#airportChart').style('width'))
+        , width = width - margin.left - margin.right
+        , mapRatio = .5
+        , height = width * mapRatio;
 
       // Append Div for tooltip to SVG
       var div = d3.select("body")
@@ -11,15 +16,21 @@
           .style("opacity", 0);
 
       var svg = d3.select("#airportChart")
+          .append("div")
+          .attr("class","svg-container")
           .append("svg")
-          .attr("width", width + margin)
-          .attr("height", height + margin)
+          //.attr("width", width)
+          //.attr("height", height)
+          .attr("preserveAspectRatio", "xMidYMid meet")
+          .attr("viewBox", "0 0 "+width + " " + height)
+          .classed("svg-content-responsive",true)
           .append('g')
           .attr('class', 'map');
 
       var projection = d3.geo.albersUsa()
                              .translate([width/2,height/2])
-                             .scale([1000]);
+                             //.scale([1000]);
+                             .scale(width);
 
       var path = d3.geo.path().projection(projection)
 
@@ -32,12 +43,13 @@
                    .style('stroke', 'black')
                    .style('stroke-width', 0.5);
 
+
       // Pop up dialog to alert user if selecting airports before 2003
       // delay type catgories not available before 2003
       function popup(d){
-          console.log(d);
           if(d.Year < 2003){
-            alert("No delay type data for any airport for years before 2003.")
+            chart_delays(d);
+            alert("No delay type data for any airport for years before 2003.");
           }
           else{
             chart_delays(d);
@@ -46,45 +58,48 @@
 
       // add a bar chart to show delay category counts
       function chart_delays(data){
-        var margin = {top:20,right:20,bottom:30,left:60},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+        var margin = {top:20,right:20,bottom:30,left:60}
+        , width = parseInt(d3.select('#airportBar').style('width'))
+        , mapRatio = 1
+        , height = width * mapRatio;
 
         var x = d3.scale.ordinal()
                   .rangeBands([0,width]);
         var y = d3.scale.linear()
                   .range([height,0]);
 
-        var svg = d3.select("#airportBar").select("svg");
+        var svg = d3.select("#airportBar").select(".svg-container-bar");
 
         if(!svg.empty()){
           svg.remove();
         }
 
-        var svg = d3.select("#airportBar").append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .append("g")
-                    .attr("transform",
-                        "translate(" + margin.left + "," + margin.top + ")");
+        var svg = d3.select("#airportBar")
+                    .append("div")
+                    .attr("class","svg-container-bar")
+                    .append("svg")
+                    //.attr("width", width)
+                    //.attr("height", height)
+                    .attr("preserveAspectRatio", "xMidYMid meet")
+                    .attr("viewBox", "-60 -25 "+(width+margin.left+margin.right) + " " + (height+margin.bottom+margin.top));
+                    //.classed("svg-content-responsive",true)
+                    //.append("g")
+                    //.attr("transform",
+                    //    "translate(" + margin.left + "," + margin.top + ")");
+                    //NOTE: Cannot use use transform with responsive SVG's
 
         var vals = [+data.CarrierDelay || 0,+data.WeatherDelay || 0,
                      +data.NASDelay || 0,+data.SecurityDelay || 0,
                      +data.LateAircraft || 0];
 
-        var cats = ['Carrier','Weather','NAS','Security','Late Aircraft'];
+        var cats = ['Carrier','Weather','NAS','Security','Late AC'];
 
         // remove the existing h1 element so that we do not append to it
         d3.select("#airportBarTitle").select("h1").remove();
 
-        //add h1 element with link to FAA delay type info
+        //add h1 element for bar chart title
         d3.select("#airportBarTitle").append("h1")
-          .html(data.Airport + " " + data.Year + " ")
-          .append("a")
-          .attr("href","http://aspmhelp.faa.gov/index.php/Types_of_Delay")
-          .attr("target","_blank")
-          //.html(data.Airport + " " + data.Year + " Delays by Category");
-          .html("Delays by Category");
+          .text(data.Airport + " " + data.Year + " Delays");
 
         x.domain(cats);
         y.domain([0,d3.max(vals)]);
@@ -112,7 +127,126 @@
            .attr("opacity",1)
            .call(d3.svg.axis().scale(y).orient("left"));
            //.call(d3.axisLeft(y));  //THIS IS for d3.v4
+
+        // add info for users
+        var info = d3.select("#airportBar").select("p");
+
+        if(!info.empty()){
+          info.remove();
+        }
+
+        // display a link to the delay type definitions
+        d3.select("#airportBar").append("p")
+          .html("")
+          .append("a")
+          .attr("href","http://aspmhelp.faa.gov/index.php/Types_of_Delay")
+          .attr("target","_blank")
+          //.html(data.Airport + " " + data.Year + " Delays by Category");
+          .html("Delay Categories Defined")
+          .style("font-size","20px");
+
+        chartYears(data.Airport);
       }
+
+      function chartYears(origin){
+         //Chart all years for the selected airport.  Show the
+         //number of total flights and number of delayed flights.
+         var margin = {top:20,right:20,bottom:30,left:60}
+         , width = parseInt(d3.select('#airportLine').style('width'))
+         , height = width / 4;
+         // format years to last two digits so we do not overcrowd the axis on small screens
+         var format = d3.format("02d");
+         d3.csv("data/aggregatedAirportData.csv", function(d){
+           d['Year'] = d['Year'].slice(2);
+           d['Year'] = d['Year'];
+           d['Total_Flts'] = +d['Total_Flts'];
+           d['Delayed_Flts'] = +d['Delayed_Flts'];
+           return d;
+         },function(error, data){
+             if (error) throw error;
+             data = data.filter(function(row){
+               return row.airport == origin;
+             });
+             var years =[],tFlights=[],dFlights=[];
+             data.forEach(function(d){
+               years.push(format(d.Year));
+               tFlights.push(d.Total_Flts);
+               dFlights.push(d.Delayed_Flts);
+             })
+             var x = d3.scale.ordinal()
+                       .rangeBands([0,width]);
+             var y = d3.scale.linear()
+                       .range([height,0]);
+
+             var svg = d3.select("#airportLine").select(".svg-container-bar");
+
+             if(!svg.empty()){
+               svg.remove();
+             }
+
+             // remove the existing h1 element so that we do not append to it
+             d3.select("#airportLineTitle").select("h1").remove();
+
+             //add h1 element with line chart title
+             d3.select("#airportLineTitle").append("h1")
+               .text(origin + " Flights (all years)");
+             var svg = d3.select("#airportLine")
+                         .append("div")
+                         .attr("class","svg-container-bar")
+                         .append("svg")
+                         //.attr("width", width)
+                         //.attr("height", height)
+                         .attr("preserveAspectRatio", "xMidYMid meet")
+                         .attr("viewBox", "-60 -25 "+(width+margin.left+margin.right) + " " + (height+margin.bottom+margin.top));
+
+           x.domain(years);
+           y.domain([0,d3.max(tFlights)]);
+           // line for total flights
+           var tLine = d3.svg.line()
+                             .x(function(d,i){return x(years[i]);})
+                             .y(function(d,i){return y(tFlights[i]);});
+           // line for delayed flights
+           var dLine = d3.svg.line()
+                             .x(function(d,i){return x(years[i]);})
+                             .y(function(d,i){return y(dFlights[i]);});
+            // add the total flights line to the svg
+            svg.append("path")
+               .attr("class","line")
+               .attr("d", tLine(tFlights))
+               .attr("data-legend",function(d) { return "Total Flights"})
+               .attr("data-legend-color","green")
+               .style("stroke","green")
+               .style("stroke-width",2);
+            // add the delayed flights line to the svg
+            svg.append("path")
+               .attr("class","line")
+               .attr("d", dLine(dFlights))
+               .attr("data-legend",function(d) { return "Delayed Flights"})
+               .attr("data-legend-color","red")
+               .style("stroke","red")
+               .style("stroke-width",2);
+            // add the axes
+            svg.append("g")
+               .attr("fill","white")
+               .attr("opacity",1)
+               .attr("transform", "translate(0," + height + ")")
+               .call(d3.svg.axis().scale(x).orient("bottom"));
+               //.call(d3.axisBottom(x));  //THIS IS for d3.v4
+            svg.append("g")
+               .attr("fill","white")
+               .attr("opacity",1)
+               .call(d3.svg.axis().scale(y).orient("left"));
+           // add a legend to the line chart
+           var legend = svg.append("g")
+                       .attr("class","legend")
+                       .attr("transform","translate(50,30)")
+                       .style("font-size","20px")
+                       .attr("data-style-padding",10)
+                       .call(d3.legend);
+
+          })
+      }
+
 
       // modeled from the udacity world-cup example.  Takes in data from
       // a csv file and draws the data on a map.
@@ -178,7 +312,7 @@
 
           // determine the radius of a circle by the max delay
           var radius = d3.scale.sqrt()
-                         .domain([0,maxDelay])
+                         .domain([0,maxFlight])
                          .range([0,12]);
 
           // create a scale for colors
@@ -207,7 +341,7 @@
              })
              .attr("r",function(d){
                //console.log(d["values"][0]["Total_Flts"]);
-               return radius(d["values"][0]["PercentDelayed"]);
+               return radius(d["values"][0]["Total_Flts"]);
              })
              .attr("fill",function(d){
                return colorFill(d["values"][0]["PercentDelayed"]);
@@ -218,10 +352,10 @@
 
           // add a colorbar so that we have a scale for the circle colors
           var colorbar = Colorbar()
-               .origin([850,20])
+               .origin([width-35,20])
                .thickness(100)
                .scale(colorFill)
-               .barlength(500).thickness(20)
+               .barlength(height).thickness(20)
                .orient("vertical")
                .title("Percent Delayed");
 
@@ -247,7 +381,7 @@
                    .attr("cx",function(d) { return d["x"];})
                    .attr("cy",function(d) { return d["y"];})
                    .attr("r",function(d){
-                     return radius(d["PercentDelayed"]);
+                     return radius(d["Total_Flts"]);
                    })
                    .on("click",popup)
                    .attr("fill",function(d){
@@ -261,7 +395,7 @@
                     div.transition()
                          .duration(200)
                           .style("opacity", .9);
-                          div.text(d.Airport+'\n'+d.PercentDelayed+'% Delayed')
+                          div.html(d.Airport+'</br>'+d.PercentDelayed+'% Delayed'+'</br>'+d.Total_Flts+' Total Flights')
                           //div.text(d.PercentDelayed)
                           .style("left", (d3.event.pageX) + "px")
                           .style("top", (d3.event.pageY - 28) + "px");
@@ -288,39 +422,33 @@
             if(year_index >= years.length){
               clearInterval(year_interval);
               // add h1 to let user know they can select a circle
-              d3.select("#airportBarTitle")
-                .append("h1")
-                .text("Click a circle!");
-              // after animation, provide buttons for user to select year
-              var buttons = d3.select("#buttonColumn")
-                              .style("overflow-y","scroll")
-                              .append("div")
-                              .attr("class", "years_buttons")
-                              .selectAll("div")
-                              .data(years)
-                              .enter()
-                              .append("div")
-                              .text(function(d){ return d;});
+               d3.select("#airportBarTitle")
+                 .append("h1")
+                 .text("Click a circle!");
+              //add h4 to let user know they can select a year
+              d3.select("#selectHolder")
+                .append("h4")
+                .style("color","white")
+                .text("Select a year");
 
-              buttons.on("click",function(d){
-                  d3.select(".years_buttons")
-                    .selectAll("div")
-                    .transition()
-                    .duration(400)
-                    .style("background-color", "black")
-                    .style("color", "white");
+              // after animation, provide drop down for user to select year
+              var select = d3.select("#selectHolder")
+                             .append("select")
+              var options = select.selectAll("option")
+                                  .data(years)
+                                  .enter()
+                                  .append("option")
+                                  .text(function(d){ return d;});
 
-                  d3.select(this)
-                    .transition()
-                    .duration(400)
-                    .style("background", "lightBlue")
-                    .style("color", "white");
-
-                  update(d);
+              select.on("change", function(d){
+                var selectedIndex = select.property("selectedIndex");
+                var data = options[0][selectedIndex].text;
+                //console.log(data);
+                update(+data);
               })
-            }
-          },1500) //1200
-      };
+             }
+           },1200) //1200
+       };
 
       // D3 to load in the csv file
       d3.csv("data/aggregatedAirportData.csv", function(d){
@@ -333,6 +461,29 @@
         d['PercentDelayed'] = d3.format(".2f")(+d['PercentDelayed'])*100;
         return d;
       },plot_points)
+
+      // d3.select(window).on('resize', resize);
+      //
+      // function resize(){
+      //     // adjust things when the window size changes
+      //     width = parseInt(d3.select('#airportChart').style('width'));
+      //     width = width - margin.left - margin.right;
+      //     height = width * mapRatio;
+      //
+      //     // update projection
+      //     projection
+      //     .translate([width / 2, height / 2])
+      //     .scale(width);
+      //
+      //     // resize the map container
+      //     svg
+      //     .style('width', width + 'px')
+      //     .style('height', height + 'px');
+      //
+      //     // resize the map
+      //     svg.select('.land').attr('d', path);
+      //     svg.selectAll('.state').attr('d', path);
+      // }
 
     };
   /*
